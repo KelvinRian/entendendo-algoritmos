d2e8cb1876f49865b598ad6e9a0fc019ac26adc5
KelvinRian <kelvinlourenco18@gmail.com>
2025-12-29 18:37:49 -0300

    Iniciada implementacao para Pesquisa Binária

--- EntendendoAlgoritmos/1.PesquisaBinária/PesquisaBinaria.cs
+++ EntendendoAlgoritmos/1.PesquisaBinária/PesquisaBinaria.cs
@@ -2,5 +2,23 @@
 {
     public class PesquisaBinaria
     {
+        public int? BuscarPosicaoNaLista(int item, List<int> lista)
+        {
+            var itemNaPrimeiraPosicao = lista[0];
+            var itemNaUltimaPosicao = lista[lista.Count - 1];
+            var itemNoMeio = lista[lista.Count / 2];
+
+            if (item == itemNoMeio)
+            {
+                return lista.IndexOf(itemNoMeio);
+            }
+
+            if (item > itemNoMeio)
+            {
+                // TODO
+            }
+
+            return null;
+        }
     }
 }
--- EntendendoAlgoritmosTests/1.PesquisaBináriaTests/PesquisaBinariaTests.cs
+++ EntendendoAlgoritmosTests/1.PesquisaBináriaTests/PesquisaBinariaTests.cs
@@ -1,6 +1,36 @@
-﻿namespace EntendendoAlgoritmosTests._1.PesquisaBináriaTests
+﻿using EntendendoAlgoritmos._1.PesquisaBinária;
+
+namespace EntendendoAlgoritmosTests._1.PesquisaBináriaTests
 {
     public class PesquisaBinariaTests
     {
+        [Fact]
+        public void DeveEncontrarItemNoMeioDaLista()
+        {
+            var lista = new List<int> { 1, 3, 5, 7, 9 };
+
+            var itemDoMeio = lista[2];
+
+            var result = new PesquisaBinaria().BuscarPosicaoNaLista(itemDoMeio, lista);
+
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void DeveEncontrarItemAcimaDoMeio()
+        {
+            var lista = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var item = 8;
+
+            var result = new PesquisaBinaria().BuscarPosicaoNaLista(item, lista);
+
+            var posicaoEsperada = lista.IndexOf(item);
+            Assert.Equal(posicaoEsperada, result);
+        }
+
+        // TODO - DeveEncontrarItemAbaixoDoMeio
+        // TODO - DeveEncontrarItemNaPrimeiraPosicao
+        // TODO - DeveEncontrarItemNaUltimaPosicao
+        // TODO - DeveRetornarNuloQuandoItemNaoExistirNaLista
     }
 }
