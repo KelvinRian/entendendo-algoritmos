9a4d954dfba963375d49460cb9093ca5625ccfab
KelvinRian <kelvinlourenco18@gmail.com>
2026-02-03 17:46:08 -0300

    implementado exemplo de recursao

---
+++ EntendendoAlgoritmosTests/0.Recurssao/ExemploRecurssaoTests.cs
@@ -0,0 +1,24 @@
+﻿using EntendendoAlgoritmos._0.Recurssao;
+
+namespace EntendendoAlgoritmosTests._0.Recurssao
+{
+    public class ExemploRecurssaoTests
+    {
+        [Fact]
+        public void DeveRetornarNoCasoBase()
+        {
+            var resultado = ExemploRecurssao.Fatorial(1);
+            var resultadoEsperado = 1;
+            Assert.Equal(resultadoEsperado, resultado);
+        }
+
+        [Fact]
+        public void DeveRetornarFatorialChamandoCasoRecurssivo()
+        {
+            var resultado = ExemploRecurssao.Fatorial(3);
+            var resultadoEsperado = 6;
+            Assert.Equal(resultadoEsperado, resultado);
+        }
+
+    }
+}
