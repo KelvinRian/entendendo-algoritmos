3f117d942a30fea74b7c3235faed368824323c28
KelvinRian <kelvinlourenco18@gmail.com>
2026-02-07 21:26:43 -0300

    implementado Quicksort

---
+++ EntendendoAlgoritmosTests/3.QuicksortTests/QuicksortTests.cs
@@ -0,0 +1,61 @@
+﻿using EntendendoAlgoritmos._3.Quicksort;
+
+namespace EntendendoAlgoritmosTests._3.QuicksortTests
+{
+    public class QuicksortTests
+    {
+        [Fact]
+        public void QuicksortArrayVazio()
+        {
+            // Arrange
+            var arrayVazio = new int[] { };
+
+            // Act
+            var resultado = Quicksort.RunQuicksort(arrayVazio);
+
+            // Assert
+            Assert.Empty(resultado);
+        }
+
+        [Fact]
+        public void QuicksortArrayComUmElemento()
+        {
+            // Arrange
+            var arrayVazio = new int[] { 1 };
+
+            // Act
+            var resultado = Quicksort.RunQuicksort(arrayVazio);
+
+            // Assert
+            Assert.Equal(arrayVazio, resultado);
+        }
+
+        [Fact]
+        public void QuicksortArrayComDoisElementos()
+        {
+            // Arrange
+            var array = new int[] { 2, 1 };
+            var resultadoEsperado = new int[] { 1, 2 };
+
+            // Act
+            var resultado = Quicksort.RunQuicksort(array);
+
+            // Assert
+            Assert.Equal(resultadoEsperado, resultado);
+        }
+
+        [Fact]
+        public void QuicksortArrayComTresElementos()
+        {
+            // Arrange
+            var array = new int[] { 5, 2, 1, };
+            var resultadoEsperado = new int[] { 1, 2, 5 };
+
+            // Act
+            var resultado = Quicksort.RunQuicksort(array);
+
+            // Assert
+            Assert.Equal(resultadoEsperado, resultado);
+        }
+    }
+}
