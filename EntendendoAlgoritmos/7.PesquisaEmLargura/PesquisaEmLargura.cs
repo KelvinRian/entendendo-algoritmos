70694d0f28af3267cc3f22cd0d375c46d1e202b1
KelvinRian <kelvinlourenco18@gmail.com>
2026-08-15 07:16:57 -0300

    implementado grafo

---
+++ EntendendoAlgoritmos/7.PesquisaEmLargura/PesquisaEmLargura.cs
@@ -0,0 +1,31 @@
+﻿using EntendendoAlgoritmos._6.Grafo;
+
+namespace EntendendoAlgoritmos._7.PesquisaEmLargura
+{
+    public class PesquisaEmLargura
+    {
+        public Grafo Grafo { get; set; }
+
+        public PesquisaEmLargura(Grafo grafo)
+        {
+            Grafo = grafo;
+        }
+
+        public int ObterMenorCaminhoEntre(Vertice inicio, Vertice fim)
+        {
+            //var filaDePesquisa = Grafo.Conexoes[inicio];
+            //var caminhoMinimo = 1;
+            //var verticesVerificados = new List<Vertice> { inicio };
+            //foreach (var vertice in filaDePesquisa)
+            //{
+            //    verticesVerificados.Add(vertice);
+            //    if (vertice == fim)
+            //    {
+            //        return caminhoMinimo;
+            //    }
+            //}
+            //return 0;
+            return 0;
+        }
+    }
+}
---
+++ EntendendoAlgoritmosTests/7.PesquisaEmLarguraTests/PesquisaEmLarguraTests.cs
@@ -0,0 +1,36 @@
+﻿using EntendendoAlgoritmos._6.Grafo;
+using EntendendoAlgoritmos._7.PesquisaEmLargura;
+
+namespace EntendendoAlgoritmosTests._7.PesquisaEmLarguraTests
+{
+    public class PesquisaEmLarguraTests
+    {
+        [Fact]
+        public void DeveObterMenorCaminhoEntreDoisVerticesDeUmGrafo()
+        {
+            //Visualização do grafo implementado
+            // A - B - D
+            //     |   |
+            //     C - E
+            var a = new Vertice { Name = "A" };
+            var b = new Vertice { Name = "B" };
+            var c = new Vertice { Name = "C" };
+            var d = new Vertice { Name = "D" };
+            var e = new Vertice { Name = "E" };
+            var conexoes = new Dictionary<Vertice, IList<Vertice>>
+            {
+                { a, new List<Vertice> { b } },
+                { b, new List<Vertice> { a, c, d } },
+                { c, new List<Vertice> { b, e } },
+                { d, new List<Vertice> { b, e } },
+                { e, new List<Vertice> { c, d } }
+            };
+
+            var grafo = new Grafo(conexoes);
+            var pesquisaEmLargura = new PesquisaEmLargura(grafo);
+            
+            var menorCaminho = pesquisaEmLargura.ObterMenorCaminhoEntre(a, d);
+            Assert.Equal(2, menorCaminho);
+        }
+    }
+}
