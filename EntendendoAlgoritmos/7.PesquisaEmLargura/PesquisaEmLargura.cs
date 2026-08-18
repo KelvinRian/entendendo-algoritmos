f91f1b9a21fdfcb6fede150527e9a12ac0f2c215
KelvinRian <kelvinlourenco18@gmail.com>
2026-08-18 18:13:08 -0300

    Adiciona Pesquisa Em Largura

--- EntendendoAlgoritmos/7.PesquisaEmLargura/PesquisaEmLargura.cs
+++ EntendendoAlgoritmos/7.PesquisaEmLargura/PesquisaEmLargura.cs
@@ -2,30 +2,72 @@
 
 namespace EntendendoAlgoritmos._7.PesquisaEmLargura
 {
-    public class PesquisaEmLargura
+    public static class PesquisaEmLargura
     {
-        public Grafo Grafo { get; set; }
-
-        public PesquisaEmLargura(Grafo grafo)
+        public static int ObterMenorCaminhoEntre(Vertice inicio, Vertice fim, Grafo grafo)
         {
-            Grafo = grafo;
-        }
+            //Cria uma fila de pesquisa onde cada elemento é uma tupla com o vértice a ser analisado
+            //e a distância dele em relação ao vértice inicio.
+            var filaDePesquisa = new Queue<(Vertice vertice, int distancia)>();
 
-        public int ObterMenorCaminhoEntre(Vertice inicio, Vertice fim)
-        {
-            //var filaDePesquisa = Grafo.Conexoes[inicio];
-            //var caminhoMinimo = 1;
-            //var verticesVerificados = new List<Vertice> { inicio };
-            //foreach (var vertice in filaDePesquisa)
-            //{
-            //    verticesVerificados.Add(vertice);
-            //    if (vertice == fim)
-            //    {
-            //        return caminhoMinimo;
-            //    }
-            //}
-            //return 0;
-            return 0;
+            //Adiciona o vértice inicial na fila de pesquisa com distância 0.
+            filaDePesquisa.Enqueue((inicio, 0));
+
+            //Uma lista com vertices verificados para não causar looping infinito na pesquisa.
+            var verticesVerificados = new HashSet<Vertice>();
+
+            //Dentro desse while a fila de pesquisa remove cada vertice que é analisado,além
+            //de adicionar os vizinhos desse vertice, caso ele não seja o vértice fim.
+            //Chegar à 0 significa que todos os vértices e seus vizinhos foram analisados.
+            //Se após esse while não for encontrado um caminho mínimo, não existe um caminho entre os vértices inicio e fim.
+            while (filaDePesquisa.Count > 0)
+            {
+                //Remove o primeiro elemento da fila de pesquisa
+                var (vertice, distancia) = filaDePesquisa.Dequeue();
+
+                //Se o vertice em questão já foi verificado, passa para o próximo da fila
+                if (verticesVerificados.Contains(vertice))
+                {
+                    continue;
+                }
+                //Se não, continua a análise
+                else
+                {
+                    //Adiciona o vértice à lista de vértices verificados
+                    verticesVerificados.Add(vertice);
+
+                    //Se o vértice atual for o vértice fim, retorna a distância até ele
+                    //A distância é calculada no fim do while.
+                    //Caso o vertice fim seja o primeiro da fila, a distância seria 0, setada no início do método,
+                    //por isso não é necessário um cálculo de distância nesse momento.
+                    if (vertice == fim)
+                    {
+                        return distancia;
+                    }
+                    //Se não, adiciona os vizinhos dele à fila de pesquisa
+                    else
+                    {
+                        //Obtém os vizinhos do vértice atual a partir do grafo
+                        var vizinhos = grafo.Conexoes[vertice];
+
+                        //Adiciona os vizinhos à fila
+                        foreach (var vizinho in vizinhos)
+                        {
+                            if (!verticesVerificados.Contains(vizinho))
+                            {
+                                //Aqui é onde a distância é setada.
+                                //A distância do vizinho em relação ao vértice de início será a distância do vértice atual + 1
+                                
+                                //Exemplo: Vértice de íncio tem distância 0, seus vizinhos terão distância 1.
+                                //Quando analisados esses vértices com distância 1, seus vizinhos terão distância 2 e assim por diante.
+                                filaDePesquisa.Enqueue((vizinho, distancia + 1));
+                            }
+                        }
+                    }
+                }
+            }
+
+            throw new Exception($"Não existe um caminho entre {inicio.Name} e {fim.Name}.");
         }
     }
 }
--- EntendendoAlgoritmosTests/7.PesquisaEmLarguraTests/PesquisaEmLarguraTests.cs
+++ EntendendoAlgoritmosTests/7.PesquisaEmLarguraTests/PesquisaEmLarguraTests.cs
@@ -27,10 +27,51 @@ namespace EntendendoAlgoritmosTests._7.PesquisaEmLarguraTests
             };
 
             var grafo = new Grafo(conexoes);
-            var pesquisaEmLargura = new PesquisaEmLargura(grafo);
             
-            var menorCaminho = pesquisaEmLargura.ObterMenorCaminhoEntre(a, d);
+            var menorCaminho = PesquisaEmLargura.ObterMenorCaminhoEntre(a, d, grafo);
             Assert.Equal(2, menorCaminho);
+        }
+
+        [Fact]
+        public void DeveObterMenorCaminhoQuandoVerticeDeIncioEFimForemOMesmo()
+        {
+            //Visualização do grafo implementado
+            // A
+            var a = new Vertice { Name = "A" };
+            var conexoes = new Dictionary<Vertice, IList<Vertice>>
+            {
+                { a, new List<Vertice>() },
+            };
+
+            var grafo = new Grafo(conexoes);
+
+            var menorCaminho = PesquisaEmLargura.ObterMenorCaminhoEntre(a, a, grafo);
+            Assert.Equal(0, menorCaminho);
+        }
+
+        [Fact]
+        public void DeveObterExceptionQuandoNaoHaCaminho()
+        {
+            //Visualização do grafo implementado
+            // A - B
+            //     C 
+            var a = new Vertice { Name = "A" };
+            var b = new Vertice { Name = "B" };
+            var c = new Vertice { Name = "C" };
+            var conexoes = new Dictionary<Vertice, IList<Vertice>>
+            {
+                { a, new List<Vertice> { b } },
+                { b, new List<Vertice> { a } },
+                { c, new List<Vertice>() },
+            };
+
+            var grafo = new Grafo(conexoes);
+
+            var exception = Assert.Throws<Exception>(() =>
+                PesquisaEmLargura.ObterMenorCaminhoEntre(a, c, grafo)
+            );
+
+            Assert.Equal("Não existe um caminho entre A e C.", exception.Message);
         }
     }
 }
